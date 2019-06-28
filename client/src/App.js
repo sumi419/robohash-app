e3a3722c889d89e23df9c874d9527c853c5a015e
sumi419 <sasgar04@gmail.com>
2019-06-28 14:20:30 -0700

    👩‍💻 Update Imports in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
-import UserInput from './components/UserInput';
+import Robots from './components/Robots';
 
 import css from './styles/app.module.css';
 
 export default function App() {
   return (
-    <>
-      <div className={css.container}>
-        <div>
-          <h1>Robots</h1>
-          <UserInput />
-        </div>
-      </div>
-    </>
+    <div className={css.container}>
+      <h1>Robots</h1>
+      <Robots />
+    </div>
   );
 }
