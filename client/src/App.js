4ff77ea687f47d9caa7eb3092d4435422e3b1a8f
sumi419 <sasgar04@gmail.com>
2019-06-28 16:04:15 -0700

    👩‍💻 Update Title in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import css from './styles/app.module.css';
 export default function App() {
   return (
     <div className={css.container}>
-      <h1>Robots</h1>
+      <h1 className={css.title}>Robots</h1>
       <Robots />
     </div>
   );
