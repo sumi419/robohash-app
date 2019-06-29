1c05fda9cd1a2b8b26394d1610d8ab52639e7fd5
sumi419 <sasgar04@gmail.com>
2019-06-28 18:21:58 -0700

    ✨ Add Heroku configuration

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like ur main.js file, or main.
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = app.listen(process.env.PORT || 3005);
