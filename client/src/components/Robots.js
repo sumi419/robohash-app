fabc9fca683ed219b8aa1f2205649c8a8c570362
sumi419 <sasgar04@gmail.com>
2019-07-15 17:58:45 -0500

    🌺 Add name to Robot

--- client/src/components/Robots.js
+++ client/src/components/Robots.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 import Robot from './Robot';
 import { svg } from '../services/svgURL';
@@ -7,6 +7,7 @@ import { svg } from '../services/svgURL';
 import css from '../styles/robots.module.css';
 
 export default function Robots() {
+  const [name, setName] = useState('');
   const [userInput, set] = useState('');
   const [image, setImage] = useState('');
   const [button, setButton] = useState('Generate');
@@ -15,16 +16,20 @@ export default function Robots() {
     e.preventDefault();
     setImage(svg);
     setButton('Generating...');
-    axios.get(`https://robohash.org/${userInput}.png`).then(() =>
-      setTimeout(() => {
-        setImage(`https://robohash.org/${userInput}.png`);
-        setButton('Generate');
-      }, 1000)
-    );
+    // axios.get(`https://robohash.org/${userInput}.png`).then(() =>
+    // convert to a buffer/blob that can be converted to the image
+    setTimeout(() => {
+      setImage(`https://robohash.org/${userInput}.png`);
+      setName(userInput);
+      setButton('Generate');
+    }, 1000);
+    // );
     set('');
   };
 
-  const handleChange = e => set(e.target.value);
+  const handleChange = e => {
+    set(e.target.value);
+  };
 
   return (
     <div className={css.container}>
@@ -43,6 +48,7 @@ export default function Robots() {
           {button}
         </button>
       </form>
+      <h1>{name}</h1>
       <Robot image={image} />
     </div>
   );
